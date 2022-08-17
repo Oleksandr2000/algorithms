35697f863e20d40277e54e12b1e03877c6520303
Oleksandr <88oleksandr@gmail.com>
2022-08-17 11:30:25 +0300

    add graph

---
+++ _breadthFirstSearch.js
@@ -0,0 +1,43 @@
+// Маємо граф з вершинами A B C D E P X R T Y U I
+
+// Найти чи існує шлях від точки A в точку I
+
+// Для зручності у програмуванні данний граф буде краще за все представити у вигляді обьекта
+
+// Ключами будуть вершини, а значенням вершини в які ми можемо потрапити
+
+// Загальний алгоритм вирішення такої задачі в контексті графів називається пошук в ширину
+
+// Складність алгоритму O(V);
+
+const graph = {
+  A: ['B', 'C', 'D'],
+  B: ['E'],
+  C: ['D', 'R'],
+  D: ['P'],
+  E: ['X'],
+  X: ['I'],
+  P: ['T'],
+  R: ['Y'],
+  T: ['U', 'I'],
+  Y: ['U'],
+  U: ['I'],
+};
+
+function searchMinPath(graph, start, end) {
+  let queue = [];
+  queue.push(start);
+  while (queue.length > 0) {
+    const currentItem = queue.shift();
+    if (!graph[currentItem]) {
+      graph[currentItem] = [];
+    }
+    if (graph[currentItem].includes(end)) {
+      return true;
+    } else {
+      queue = [...queue, ...graph[currentItem]];
+    }
+  }
+}
+
+console.log(searchMinPath(graph, 'A', 'I'));
